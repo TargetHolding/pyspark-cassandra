536390840d948599bc15f86517917619b7b524e5
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2015-04-27 12:38:37 +0200

    added test for map types

--- src/test/python/pyspark_cassandra/it_suite.py
+++ src/test/python/pyspark_cassandra/it_suite.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from decimal import Decimal
+import string
 import sys
 import unittest
 import uuid
@@ -13,7 +14,7 @@ from pyspark_cassandra import CassandraSparkContext, RowFormat, Row, UDT
 
 class CassandraTestCase(unittest.TestCase):
     keyspace = "test_pyspark_cassandra"
-    
+
     @classmethod
     def setUpClass(cls):
         super(CassandraTestCase, cls).setUpClass()
@@ -25,23 +26,23 @@ class CassandraTestCase(unittest.TestCase):
         ''')
         cls.session.set_keyspace('test_pyspark_cassandra')
 
-    
+
     @classmethod
     def tearDownClass(cls):
         super(CassandraTestCase, cls).tearDownClass()
         cls.session.shutdown()
         cls.sc.stop()
-        
+
 
 class SimpleTypesTest(CassandraTestCase):
     table = "simple_types"
-    
+
     simple_types = [
         'ascii', 'bigint', 'blob', 'boolean', 'decimal', 'double', 'float',
         'inet', 'int', 'text', 'timestamp', 'timeuuid', 'varchar', 'varint',
         'uuid',
     ]
-    
+
     @classmethod
     def setUpClass(cls):
         super(SimpleTypesTest, cls).setUpClass()
@@ -50,7 +51,7 @@ class SimpleTypesTest(CassandraTestCase):
                 key text primary key, %s
             )
         ''' % ', '.join('{0} {0}'.format(t) for t in cls.simple_types))
-    
+
     @classmethod
     def tearDownClass(cls):
         cls.session.execute('DROP TABLE simple_types')
@@ -77,62 +78,114 @@ class SimpleTypesTest(CassandraTestCase):
     def read_write_test(self, type_name, value):
         self.sc.parallelize([{ 'key': type_name, type_name: value}]).saveToCassandra(self.keyspace, self.table)
         self.read_test(type_name, value)
-        
+
     def test_ascii(self):
         self.read_write_test('ascii', 'some ascii')
 
     def test_bigint(self):
         self.read_write_test('bigint', sys.maxint)
-        
+
     def test_blob(self):
         self.read_write_test('blob', bytearray('some blob'))
-        
+
     def test_boolean(self):
         self.read_write_test('boolean', False)
-        
+
     def test_decimal(self):
         self.read_write_test('decimal', Decimal(0.5))
-        
+
     def test_double(self):
         self.read_write_test('double', 0.5)
-        
+
     def test_float(self):
         self.read_write_test('float', 0.5)
-        
+
     # TODO returns resolved hostname with ip address (hostname/ip,
     # e.g. /127.0.0.1), but doesn't accept with / ...
     #def test_inet(self):
     #    self.read_write_test('inet', u'/127.0.0.1')
-        
+
     def test_int(self):
         self.read_write_test('int', 1)
-        
+
     def test_text(self):
         self.read_write_test('text', u'some text')
-        
+
     def test_timestamp(self):
         self.read_write_test('timestamp', datetime(2015, 1, 1))
-        
+
     # Invalid version for TimeUUID type.
     def test_timeuuid(self):
         uuid = uuid_from_time(datetime(2015, 1, 1))
         self.read_write_test('timeuuid', uuid)
-        
+
     def test_varchar(self):
         self.read_write_test('varchar', u'some varchar')
-        
+
     def test_varint(self):
         self.read_write_test('varint', 1)
-        
+
     def test_uuid(self):
         self.read_write_test('uuid', uuid.UUID('22dadfd0-b971-11e4-a856-85a08dca5bbf'))
-        
-        
+
+
+class CollectionTypesTest(CassandraTestCase):
+    table = "collection_types"
+    collection_types = {
+        'm': 'map<text, text>',
+        'l': 'list<text>',
+        's': 'set<text>',
+    }
+
+    @classmethod
+    def setUpClass(cls):
+        super(CollectionTypesTest, cls).setUpClass()
+        cls.session.execute('''
+            CREATE TABLE IF NOT EXISTS %s (
+                key text primary key, %s
+            )
+        ''' % (cls.table, ', '.join('%s %s' % (k, v) for k, v in cls.collection_types.items())))
+
+    @classmethod
+    def tearDownClass(cls):
+        # cls.session.execute('DROP TABLE %s' % cls.table)
+        super(CollectionTypesTest, cls).tearDownClass()
+
+    def setUp(self):
+        super(CollectionTypesTest, self).setUp()
+        self.session.execute('TRUNCATE %s' % self.table)
+
+    def test_map(self):
+        maps = [
+            {
+                'key': 'm%s' % i,
+                'm': { k : 'x' for k in string.ascii_lowercase[:i] }
+            } for i in range(1, 10)
+        ]
+
+        maps_by_key = {
+            m['key'] : m['m']
+            for m in maps
+        }
+
+        # requires no. partitions to equal the no. elements in maps
+        # PickleRowWriter.unpickle(...) doesn't accept batched unpickling just yet
+        self.sc.parallelize(maps, len(maps)).saveToCassandra(self.keyspace, self.table)
+
+        rdd = self.sc.cassandraTable(self.keyspace, self.table).select('key', 'm').cache()
+        self.assertEqual(len(maps), rdd.count())
+
+        collected = rdd.collect()
+        self.assertEqual(len(maps), len(collected))
+
+        for row in collected:
+            self.assertEqual(maps_by_key[row.key], row.m)
+
 if __name__ == '__main__':
     unittest.main()
 
-# 
-# 
+#
+#
 # session.execute('''
 #     CREATE TABLE IF NOT EXISTS collection_types (
 #         key text primary key,
@@ -141,25 +194,25 @@ if __name__ == '__main__':
 #         l list<text>
 #     )
 # ''')
-# 
+#
 # session.execute('''
 #     CREATE TABLE IF NOT EXISTS counter (
 #         key text primary key,
 #         counter counter,
 #     )
 # ''')
-# 
+#
 # session.execute('''
 #     CREATE TYPE IF NOT EXISTS test_udt (
 #         text text,
 #         int int
 #     )
 # ''')
-# 
+#
 # session.execute('''
 #     CREATE TABLE IF NOT EXISTS udt_type (
 #         key text primary key,
 #         udt frozen<test_udt>
 #     )
 # ''')
-# 
+#
