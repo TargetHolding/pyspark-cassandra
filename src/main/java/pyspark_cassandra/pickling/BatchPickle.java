520e32f314ccfee76640efe5a8dcca8b5d94de6a
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2015-05-02 02:00:33 +0200

    improving performance in batching reads

--- src/main/java/pyspark_cassandra/pickling/BatchPickle.java
+++ src/main/java/pyspark_cassandra/pickling/BatchPickle.java
@@ -2,9 +2,9 @@ package pyspark_cassandra.pickling;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.Arrays;
 import java.util.Iterator;
 
+import net.razorvine.pickle.PickleException;
 import net.razorvine.pickle.custom.Pickler;
 
 import org.apache.spark.api.java.function.FlatMapFunction;
@@ -13,18 +13,55 @@ import pyspark_cassandra.types.Types;
 
 public class BatchPickle implements FlatMapFunction<Iterator<Object>, byte[]> {
 	private static final long serialVersionUID = 1L;
-	
+
+	private static final int DEFAULT_BATCH_SIZE = 1000;
+
 	static {
 		Types.registerCustomTypes();
 	}
 
 	private transient Pickler pickler = new Pickler();
+	private int batchSize;
+
+	public BatchPickle() {
+		this(DEFAULT_BATCH_SIZE);
+	}
+
+	public BatchPickle(int batchSize) {
+		this.batchSize = batchSize;
+	}
 
 	@Override
 	public Iterable<byte[]> call(Iterator<Object> partition) throws Exception {
-		return Arrays.asList(new byte[][] { this.pickler.dumps(partition) });
+		final BatchingIterator<Object> bi = new BatchingIterator<>(partition, this.batchSize);
+
+		return new Iterable<byte[]>() {
+			@Override
+			public Iterator<byte[]> iterator() {
+				return new Iterator<byte[]>() {
+					@Override
+					public boolean hasNext() {
+						return bi.hasNext();
+					}
+
+					@Override
+					public byte[] next() {
+						try {
+							return pickler.dumps(bi.next());
+						} catch (PickleException | IOException e) {
+							throw new RuntimeException(e);
+						}
+					}
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException();
+					}
+				};
+			}
+		};
 	}
-	
+
 	private void readObject(ObjectInputStream s) throws ClassNotFoundException, IOException {
 		s.defaultReadObject();
 		this.pickler = new Pickler();
