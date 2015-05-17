efe7929c56845c0f1272d652933da68dc754b5d0
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2015-05-17 18:53:30 +0200

    support construcing LWRows from lists of fields names and values

--- src/main/java/pyspark_cassandra/types/LWRow.java
+++ src/main/java/pyspark_cassandra/types/LWRow.java
@@ -1,5 +1,8 @@
 package pyspark_cassandra.types;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LWRow {
 	private String[] fields;
 	private Object[] values;
@@ -9,11 +12,21 @@ public class LWRow {
 		this.values = values;
 	}
 
+	public LWRow(List<String> fields, List<Object> values) {
+		// TODO consider another approach to remove this conceptually unnecessary toArray calls
+		this(fields.toArray(new String[fields.size()]), values.toArray());
+	}
+
 	public String[] getFieldsNames() {
 		return fields;
 	}
 
 	public Object[] getFieldValues() {
 		return values;
+	}
+
+	@Override
+	public String toString() {
+		return "LWRow [fields=" + Arrays.toString(fields) + ", values=" + Arrays.toString(values) + "]";
 	}
 }
