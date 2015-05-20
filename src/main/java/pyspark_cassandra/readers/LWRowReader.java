e4da9f582c65094d9aa238a37592eb9167223624
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2015-05-20 22:33:34 +0200

    fixed NPE in LWRowReader closes #29

--- src/main/java/pyspark_cassandra/readers/LWRowReader.java
+++ src/main/java/pyspark_cassandra/readers/LWRowReader.java
@@ -10,13 +10,16 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 
 package pyspark_cassandra.readers;
+
+import java.nio.ByteBuffer;
 
 import pyspark_cassandra.types.LWRow;
 
 import com.datastax.driver.core.ColumnDefinitions;
+import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Row;
 import com.datastax.spark.connector.cql.TableDef;
@@ -30,7 +33,11 @@ public class LWRowReader extends RowReader<LWRow> {
 		Object[] values = new Object[columnNames.length];
 
 		for (int i = 0; i < values.length; i++) {
-			values[i] = defs.getType(columnNames[i]).deserialize(row.getBytesUnsafe(columnNames[i]), protocolVersion);
+			if (!row.isNull(columnNames[i])) {
+				ByteBuffer bytes = row.getBytesUnsafe(columnNames[i]);
+				DataType type = defs.getType(columnNames[i]);
+				values[i] = type.deserialize(bytes, protocolVersion);
+			}
 		}
 
 		return new LWRow(columnNames, values);
