00f0c05209c6ee99796ef0d5709d63320560eeac
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2015-11-27 06:50:27 +0100

    add script to start the released versino

---
+++ tools/pyspark_cassandra_released.sh
@@ -0,0 +1,12 @@
+#export IPYTHON_OPTS="notebook"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+
+PYSPARK_DRIVER_PYTHON=ipython \
+	$DIR/lib/spark-1.4.1-bin-hadoop2.6/bin/pyspark \
+	--conf spark.cassandra.connection.host="localhost" \
+	--driver-memory 2g \
+	--master local[*] \
+	--packages TargetHolding/pyspark-cassandra:0.2.0
+	$@
+
