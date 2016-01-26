eb1332bb619c6a19d6814d1ca1ea97ae78fa9b34
Frens Jan Rumph <frens.jan.rumph@target-holding.nl>
2016-01-26 21:24:45 +0100

    trim the version number

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ setup(
 	name='pyspark_cassandra',
 	maintainer='Frens Jan Rumph',
 	maintainer_email='frens.jan.rumph@target-holding.nl',
-	version=f('../version.txt').read(),
+	version=f('../version.txt').read().strip(),
 	description='Utilities to asssist in working with Cassandra and PySpark.',
 	long_description=f('../README.md').read(),
 	url='https://github.com/TargetHolding/pyspark-cassandra',
